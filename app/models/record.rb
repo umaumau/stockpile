b7afaf75f8b5ceb2fc1af8985d77e130197781bd
umaumau <mako.melon@gmail.com>
2023-06-26 19:34:59 +0900

    renderデータベースリセットの記載削除

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,6 @@
 class Record < ApplicationRecord
   validates :price,       presence: true, unless: :time_minute?
-  validates :time_minute, presence: true, unless: :price?      
+  validates :time_minute, presence: true, unless: :price?
   validates :content,     presence: true
 
   belongs_to :user
