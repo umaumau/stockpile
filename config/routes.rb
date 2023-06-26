973342a128ed8789622e8086d199663e3498dcb0
umaumau <mako.melon@gmail.com>
2023-06-26 19:08:08 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "records#index"
   resources :records, only: [:index, :create, :show]
+  resources :options, only: [:index, :create, :update, :destroy]
 end
