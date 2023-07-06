274368d15b39874405b5bb6e0c26da8f3008ff0c
umaumau <mako.melon@gmail.com>
2023-07-06 19:58:16 +0900

    カレンダーの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'calendars/index'
   devise_for :users
   root to: "records#index"
   resources :records, only: [:index, :create]
   resources :options, only: [:edit, :update]
+  resources :calendars, only: :index
 end
