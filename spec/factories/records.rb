68870a77907179593b90ade2fd7cec6b7e928294
umaumau <mako.melon@gmail.com>
2023-07-24 19:42:32 +0900

    recordモデルテスト

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :record do
-    
+    price       {'100'}
+    time_minute {'10'}
+    content     {'勉強'}
+    rate        {'10'}
+    association :user
   end
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @record = FactoryBot.build(:record)
+  end
+
+  describe '記録登録' do
+    context '記録登録できるとき' do
+      it '必要な情報が存在する' do
+        expect(@record).to be_valid
+      end 
+    end
+    context '記録登録できないとき' do
+      it 'contentが空' do
+        @record.content = ''
+        @record.valid?
+        expect(@record.errors.full_messages).to include('Contentを入力してください')
+      end
+      it 'priceとtime_minuteが空' do
+        @record.price = ''
+        @record.time_minute = ''
+        @record.valid?
+        expect(@record.errors.full_messages).to include('Priceを入力してください', 'Time minuteを入力してください')
+      end
+      it 'rateが空' do
+        @record.rate = ''
+        @record.valid?
+        expect(@record.errors.full_messages).to include('Rateを入力してください')
+      end
+      it 'userが紐づいてない' do
+        @record.user = nil
+        @record.valid?
+        expect(@record.errors.full_messages).to include('Userを入力してください')
+      end
+    end
+  end
 end
